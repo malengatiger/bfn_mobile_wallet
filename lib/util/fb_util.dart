c274f7550ec977a02052c6dcd071e25c412c336b
malengatiger <malengatiger@gmail.com>
2020-10-03 11:05:14 +0200

    commit prior to move

--- lib/util/fb_util.dart
+++ lib/util/fb_util.dart
@@ -9,7 +9,8 @@ class FireBaseUtil {
     p('$FERN $FERN $FERN Firebase has been initialized: $PINK_FLOWER ${m.name} ${m.options.databaseURL}');
     return m;
   }
-  static bool isUserLoggedIn()  {
+
+  static bool isUserLoggedIn() {
     var auth = FirebaseAuth.instance;
     if (auth.currentUser != null) {
       p('$PEACH user is already logged in');
@@ -18,5 +19,14 @@ class FireBaseUtil {
     p('$ERROR $ERROR user is already logged in');
     return false;
   }
-}
 
+  static Future createAuthUser(String email, String password) async {
+    var auth = FirebaseAuth.instance;
+
+    var result = await auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    if (result.user != null) {
+      p('$PINK_FLOWER Auth User has been created: ${result.user.uid}');
+    }
+  }
+}
